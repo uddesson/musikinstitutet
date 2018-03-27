e18412304559b5558f9d1183ce146c0303639bb0
Emma Uddesson <emma.uddesson@gmail.com>
2018-03-27 13:39:16 +0200

    Add separate log-function for testing
    Groups a couple of console.log:s. Currently used only for showing fetched artists in console.

--- js/main.js
+++ js/main.js
@@ -11,8 +11,18 @@ function fetchAllArtists(){
                 /* Maybe here we should have a function like "excludeMaleArtists()";   
                 That checks if (artist != "male") before the code continues executing */
                 
+                logInfo(artists[i]); //Shows info about each fetched artists in console
             }
         });
 }
 
 fetchAllArtists();
+
+/* logInfo could be used as a helper function to console.log several things at once,
+can be further developed so we can use it for several things later! */
+function logInfo(element){ 
+    console.group("Console Log shows:");
+    console.log('id:', element._id)
+    console.log('Name:', element.name)
+    console.groupEnd();
+}
