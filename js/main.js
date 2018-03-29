b6cc1627db567965cc036fbf32a9c23c01e54152
Emma Uddesson <emma.uddesson@gmail.com>
2018-03-29 16:25:16 +0200

    Restructure our first MVC-objects
    And rename them after ideas in structureideas.txt

--- js/main.js
+++ js/main.js
@@ -1,21 +1,36 @@
-// WIP
 const apiKey = `?key=flat_eric`;
-const artistsUrl = `https://folksa.ga/api/artists${apiKey}`; 
+const url = `https://folksa.ga/api/artists${apiKey}`; // Obs! Currently just category 'artists'
+
+/******************
+ ** Controllers ***
+ ******************/
+
+const GenderController = {
+    artistIsNotMale(artist){
+        if(artist.gender !== 'male'){
+            FetchModel.logInfo(artist); // Just for seeing what's in there
+            return true;   
+        }
+        else{
+            return false;
+        }
+    }
+}
 
 
 /******************
- ***** Model? *****
+ ***** Models *****
  ******************/
-const Model = {
+const FetchModel = {
 
-    fetchArtistsThatArentMale(){
-        fetch(artistsUrl)
+    fetchArtists(){
+        fetch(url)
             .then((response) => response.json())
             .then((artists) => {
                 // Loop through all the artists
                 for (var i = 0; i < artists.length; i++) {
-                    if(Model.artistIsNotMale(artists[i]) == true){
-                        View.displayArtistName(artists[i].name); 
+                    if(GenderController.artistIsNotMale(artists[i]) == true){
+                        ArtistView.displayArtistName(artists[i].name); 
                     };     
                 }
             })
@@ -26,23 +41,13 @@ const Model = {
             });
     },
 
-    artistIsNotMale(artist){
-        if(artist.gender !== 'male'){
-            Model.logInfo(artist); // Just for seeing what's in there
-            return true;   
-        }
-        else{
-            return false;
-        }
-    },
-
     /* This logInfo-function can be used to console.log several things at once,
     and be further developed so we can use it for several things later! */
     logInfo(element){ 
         console.group("Console Log shows:");
-        console.log('id:', element._id)
-        console.log('Name:', element.name)
-        console.log('Gender:', element.gender)
+        console.log('id:', element._id);
+        console.log('Name:', element.name);
+        console.log('Gender:', element.gender);
         console.groupEnd();
     }
 } // Closing Model
@@ -50,20 +55,25 @@ const Model = {
 
 
 
-
 /******************
- ***** View? *****
+ ***** Views *****
  ******************/
-const View = {
+
+const ArtistView = {
     testList: document.getElementById('test-list'),
 
      //Testing: Displaying some output
      displayArtistName(artistname){
-        let listItem = document
-            .createElement('li');
+        let listItem = document.createElement('li');
         listItem.innerText = artistname;
-        View.testList.appendChild(listItem);
-     }
+        ArtistView.testList.appendChild(listItem);
+    }
 }
 
-Model.fetchArtistsThatArentMale();
+
+
+/**********************
+ *** Run functions! ***
+ **********************/
+
+FetchModel.fetchArtists();
