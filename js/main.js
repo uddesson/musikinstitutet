5805b2c264fb5b827b7745d6778ffecf9a50bf15
Lisa Sundberg <lisa.sundberg@hyperisland.se>
2018-04-09 14:03:05 +0200

    Print out images for artists

--- js/main.js
+++ js/main.js
@@ -258,6 +258,9 @@ const PostModel = {
 			
 			let artistDiv = document.createElement('div');
 			artistDiv.innerHTML = `
+					<div class="image-container">
+						<img src="${artist.coverImage}" alt="${artist.name}">
+					</div>
 					<h3>${artist.name}</h3>
 					<p>Genres: ${artist.genres}</p>`;
 			ArtistView.containerInner.appendChild(artistDiv);
